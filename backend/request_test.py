1df4bd4f391480c9201abb280a6a65f345d7f391
eu_mental <val.mitkv@gmail.com>
2022-06-11 00:52:45 +0300

    backend raw 1

--- backend/request_test.py
+++ backend/request_test.py
@@ -7,10 +7,12 @@ features = {
     'pickup_latitude': 40.767937,
     'dropoff_longitude': -73.964630,
     'dropoff_latitude': 40.765602,
-    'pickup_datetime': 'asdasd',
+    'pickup_datetime': '2016-03-14 17:24:55',
     'passenger_count': 1,
-    'vendor_id': 2
+    'vendor_id': 2,
+    'store_and_fwd_flag': 'N'
     }
 
 resp = requests.post(f'{path}/predict',
                     json=features)
+print(resp.json())
